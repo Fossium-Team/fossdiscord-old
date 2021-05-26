bbbcf56850b5d68c2ce54ef674da8f5551800c56
reoccurcat <84601340+reoccurcat@users.noreply.github.com>
2021-05-26 13:32:18 -0500

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -172,7 +172,8 @@ class Admin(commands.Cog):
         if str(ctx.message.author.id) == config.ownerID:
             server = self.bot.get_guild(int(serverID))
             await self.bot.leave_server(server)
-
-
+            embed = discord.Embed(title = f"Left the server '{server.name}'.")
+            await ctx.send(embed = embed)
+            
 def setup(bot):
     bot.add_cog(Admin(bot))
