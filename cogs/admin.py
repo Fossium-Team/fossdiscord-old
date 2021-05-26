7a5cbd0fb67b261d8823e98cbb542e5a01dd5d0e
reoccurcat <84601340+reoccurcat@users.noreply.github.com>
2021-05-25 21:30:43 -0500

    ufadsijfidsjflads

--- cogs/admin.py
+++ cogs/admin.py
@@ -130,7 +130,7 @@ class Admin(commands.Cog):
             #channel = discord.utils.get(server.channels, name='general')
             #await ctx.send(channel)
             for channel in server.channels:
-                if channel.name == "general":
+                if channel.name == "general" or "général":
                     await ctx.send(channel.id)
                     invite = await channel.create_invite()
                     await ctx.send(invite)
