0574f4b088aaf82556c8b2b49f113efcbb345cfe
reoccurcat <84601340+reoccurcat@users.noreply.github.com>
2021-05-25 19:49:18 -0500

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -108,7 +108,8 @@ class Admin(commands.Cog):
     async def servers(self, ctx):
         if str(ctx.message.author.id) == config.ownerID:
             servers = list(self.bot.guilds)
-            embed = discord.Embed(title = f"Connected on {str(len(servers))} servers:", description = '\n'.join(guild.name for guild in self.bot.guilds))
+            embed = discord.Embed(title = f"Connected on {str(len(servers))} servers:")
+            embed.add_field(name = "Servers", value = '\n'.join(guild.name for guild in self.bot.guilds))
             embed.add_field(name = "Server IDs", value = '\n'.join(str(guild.id) for guild in self.bot.guilds))
             await ctx.send(embed = embed)
             #await ctx.send(f"Connected on {str(len(servers))} servers:")
