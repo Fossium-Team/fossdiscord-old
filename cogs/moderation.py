6059d710260d76e102b15b9a992a762004dabcd1
reoccurcat <84601340+reoccurcat@users.noreply.github.com>
2021-05-27 18:20:25 -0500

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -32,215 +32,179 @@ class Moderation(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def purge(self, ctx, amount=10):
         """Purge messages, default amount is 10."""
-        if config.bot_lockdown_status == 'no_lockdown':
-            await ctx.channel.purge(limit=amount+1)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        await ctx.channel.purge(limit=amount+1)
+
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.Member, *reason):
         """Kick a member."""
-        if config.bot_lockdown_status == 'no_lockdown':
-            args = " ".join(reason[:])
-            if not reason:
-                await user.kick()
-                em = discord.Embed(title = f"**{user}** has been kicked, reason: **none**.")
-                await ctx.send(embed = em)
-            else:
-                await user.kick()
-                em = discord.Embed(title = f"**{user}** has been kicked, reason: **{args}**.")
-                await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        args = " ".join(reason[:])
+        if not reason:
+            await user.kick()
+            em = discord.Embed(title = f"**{user}** has been kicked, reason: **none**.")
             await ctx.send(embed = em)
+        else:
+            await user.kick()
+            em = discord.Embed(title = f"**{user}** has been kicked, reason: **{args}**.")
+            await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user: discord.Member, *reason):
         """Ban a member."""
-        if config.bot_lockdown_status == 'no_lockdown':
-            args = " ".join(reason[:])
-            if not reason:
-                await user.ban()
-                em = discord.Embed(title = f"**{user}** has been banned, reason: **none**.")
-                await ctx.send(embed = em)
-            else:
-                await user.ban()
-                em = discord.Embed(title = f"**{user}** has been banned, reason: **{args}**.")
-                await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        args = " ".join(reason[:])
+        if not reason:
+            await user.ban()
+            em = discord.Embed(title = f"**{user}** has been banned, reason: **none**.")
             await ctx.send(embed = em)
+        else:
+            await user.ban()
+            em = discord.Embed(title = f"**{user}** has been banned, reason: **{args}**.")
+            await ctx.send(embed = em)
+
 
     @commands.command() # Takes 1s 1m 1h 1d
     @commands.has_permissions(manage_messages=True)
     async def mute(self, ctx, user: discord.Member, mutetime):
         #BTW need to import time&asyncio module to work.
         """Mute a member."""
-        if config.bot_lockdown_status == 'no_lockdown':
-            if timeconvertion(mutetime) != False:
-                role = discord.utils.get(user.guild.roles, name="muted")
-                await user.add_roles(role)
-                em = discord.Embed(title = "User has been muted for " + "`{}`".format(str(mutetime)) + ".")
-                await ctx.send(embed = em)
-                await asyncio.sleep(timeconvertion(mutetime))
-                await user.remove_roles(role)
-            elif timeconvertion(mutetime) == False:
-                em = discord.Embed(title = "The time format doesn't seem right.")
-                await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        if timeconvertion(mutetime) != False:
+            role = discord.utils.get(user.guild.roles, name="muted")
+            await user.add_roles(role)
+            em = discord.Embed(title = "User has been muted for " + "`{}`".format(str(mutetime)) + ".")
             await ctx.send(embed = em)
+            await asyncio.sleep(timeconvertion(mutetime))
+            await user.remove_roles(role)
+        elif timeconvertion(mutetime) == False:
+            em = discord.Embed(title = "The time format doesn't seem right.")
+            await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def softban(self, ctx, user: discord.Member, *reason):
-        if config.bot_lockdown_status == 'no_lockdown':
-            args = " ".join(reason[:])
-            await ctx.guild.ban(user)
-            await ctx.guild.unban(user)
-            if not reason:
-                em = discord.Embed(title = f"**{user}** has been softbanned, reason: **none**.")
-                await ctx.send(embed = em)
-            else:
-                em = discord.Embed(title = f"**{user}** has been softbanned, reason: **{args}**.")
-                await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
+        args = " ".join(reason[:])
+        await ctx.guild.ban(user)
+        await ctx.guild.unban(user)
+        if not reason:
+            em = discord.Embed(title = f"**{user}** has been softbanned, reason: **none**.")
             await ctx.send(embed = em)
+        else:
+            em = discord.Embed(title = f"**{user}** has been softbanned, reason: **{args}**.")
+            await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, id: int):
         """Unban a member."""
-        if config.bot_lockdown_status == 'no_lockdown':
-            userToUnban = await self.bot.fetch_user(id)
-            await ctx.guild.unban(userToUnban)
-            em = discord.Embed(title = "Successfully unbanned `" + userToUnban.name + "`")
-            await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        userToUnban = await self.bot.fetch_user(id)
+        await ctx.guild.unban(userToUnban)
+        em = discord.Embed(title = "Successfully unbanned `" + userToUnban.name + "`")
+        await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def unmute(self, ctx, user: discord.Member):
         """Unmute a member."""
-        if config.bot_lockdown_status == 'no_lockdown':
-            role = discord.utils.get(user.guild.roles, name="muted")
-            await user.remove_roles(role)
-            em = discord.Embed(title = "Successfully unmuted `" + user.name + "`")
-            await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        role = discord.utils.get(user.guild.roles, name="muted")
+        await user.remove_roles(role)
+        em = discord.Embed(title = "Successfully unmuted `" + user.name + "`")
+        await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def warn(self, ctx, user : discord.Member, *reason):
         args = " ".join(reason[:])
-        if config.bot_lockdown_status == 'no_lockdown':
-            if not os.path.exists('warns'):
-                os.makedirs('warns')
-            try:
-                if os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size > 0:
-                    await ctx.send("Successfully warned that member")
-                    writeReasonTemplate = str(args)
-                    warns = open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py", 'a')
-                    warns.write("\n")
-                    warns.write(writeReasonTemplate)
-                    warns.close()
 
-                elif os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size == 0:
-                    await ctx.send("Successfully warned that member")
-                    writeReasonTemplate = str(args)
-                    warns = open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py", 'a')
-                    warns.write(writeReasonTemplate)
-                    warns.close()
-            except:
+        if not os.path.exists('warns'):
+            os.makedirs('warns')
+        try:
+            if os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size > 0:
+                await ctx.send("Successfully warned that member")
+                writeReasonTemplate = str(args)
+                warns = open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py", 'a')
+                warns.write("\n")
+                warns.write(writeReasonTemplate)
+                warns.close()
+
+            elif os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size == 0:
                 await ctx.send("Successfully warned that member")
                 writeReasonTemplate = str(args)
                 warns = open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py", 'a')
                 warns.write(writeReasonTemplate)
                 warns.close()
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        except:
+            await ctx.send("Successfully warned that member")
+            writeReasonTemplate = str(args)
+            warns = open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py", 'a')
+            warns.write(writeReasonTemplate)
+            warns.close()
+
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def warns(self, ctx, user : discord.Member):
-        if config.bot_lockdown_status == 'no_lockdown':
-            if not os.path.exists('warns'):
-                os.makedirs('warns')
-            try:
-                if os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size > 0:
-                    with open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py") as f:
-                        lines = f.readlines()
-                        lines_clean = "".join(lines[:])
-                        if not lines_clean:
-                            em = discord.Embed(title = "Warns for " + str(user), description = "This user has no warnings")
-                        else:
-                            em = discord.Embed(title = "Warns for " + str(user), description = lines_clean)
-                            await ctx.send(embed = em)
-                elif os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size == 0:
-                    em = discord.Embed(title = "Warns for " + str(user), description = "This user has no warnings")
-                    await ctx.send(embed = em)
-            except:
-                    em = discord.Embed(title = "Warns for " + str(user), description = "This user has no warnings")
-                    await ctx.send(embed = em)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        if not os.path.exists('warns'):
+            os.makedirs('warns')
+        try:
+            if os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size > 0:
+                with open("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py") as f:
+                    lines = f.readlines()
+                    lines_clean = "".join(lines[:])
+                    if not lines_clean:
+                        em = discord.Embed(title = "Warns for " + str(user), description = "This user has no warnings")
+                    else:
+                        em = discord.Embed(title = "Warns for " + str(user), description = lines_clean)
+                        await ctx.send(embed = em)
+            elif os.stat("warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py").st_size == 0:
+                em = discord.Embed(title = "Warns for " + str(user), description = "This user has no warnings")
+                await ctx.send(embed = em)
+        except:
+                em = discord.Embed(title = "Warns for " + str(user), description = "This user has no warnings")
+                await ctx.send(embed = em)
+
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def delwarn(self, ctx, user : discord.Member, *, reason):
-        if config.bot_lockdown_status == 'no_lockdown':
-            if not os.path.exists('warns'):
-                os.makedirs('warns')
-            fn = "warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py"
-            f = open(fn)
-            output = []
-            word=str(reason)
-            for line in f:
-                if not line.startswith(word):
-                    output.append(line)
-            f.close()
-            f = open(fn, 'w')
-            f.writelines(output)
-            f.close()
-            await ctx.send("Successfully removed that warning")
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        if not os.path.exists('warns'):
+            os.makedirs('warns')
+        fn = "warns/" + str(user.id) + "_" + str(ctx.message.guild.id) + ".py"
+        f = open(fn)
+        output = []
+        word=str(reason)
+        for line in f:
+            if not line.startswith(word):
+                output.append(line)
+        f.close()
+        f = open(fn, 'w')
+        f.writelines(output)
+        f.close()
+        await ctx.send("Successfully removed that warning")
+
     
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_nicknames=True)
     async def modnick(self, ctx, *, user: discord.Member):
-        if config.bot_lockdown_status == 'no_lockdown':
-            source = string.ascii_letters + string.digits
-            result_str = ''.join((random.choice(source) for i in range(8)))
-            newnickname = f"ModdedNick-{result_str}"
-            await user.edit(nick=newnickname)
-            await ctx.message.delete()
-            await ctx.send(f'Nickname was moderated for {user.mention} ({user.name}#{user.discriminator}).', delete_after=5.0)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        source = string.ascii_letters + string.digits
+        result_str = ''.join((random.choice(source) for i in range(8)))
+        newnickname = f"ModdedNick{result_str}"
+        await user.edit(nick=newnickname)
+        await ctx.message.delete()
+        await ctx.send(f'Nickname was moderated for {user.mention} ({user.name}#{user.discriminator}).', delete_after=5.0)
+
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_nicknames=True)
     async def changenick(self, ctx, user: discord.Member, nick):
-        if config.bot_lockdown_status == 'no_lockdown':
-            await user.edit(nick=nick)
-            await ctx.message.delete()
-            await ctx.send(f'Nickname was changed for {user.mention} ({user.name}#{user.discriminator}).', delete_after=5.0)
-        elif config.bot_lockdown_status == "lockdown_activated":
-            em = discord.Embed(title = "This bot is locked down", description = "<@!" + config.ownerID + "> has locked down this bot globally.")
-            await ctx.send(embed = em)
+        await user.edit(nick=nick)
+        await ctx.message.delete()
+        await ctx.send(f'Nickname was changed for {user.mention} ({user.name}#{user.discriminator}).', delete_after=5.0)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
