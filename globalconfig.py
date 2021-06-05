9709ca0a195b959a28ccc4d91f9464760f0d56eb
Sven <68018116+SKBotNL@users.noreply.github.com>
2021-06-05 17:13:32 +0200

    Update globalconfig.py

--- globalconfig.py
+++ globalconfig.py
@@ -1,7 +1,5 @@
 # Copyright (c) 2021 SKBotNL (and the members of the FOSS-Devs)
 # See LICENSE in the project root for license information.
 
-version = "3.0"
-github_login_url = 'https://github.com/reoccurdevs/freediscord'
 # Command list
 commands = ["about", "ban", "changenick", "delwarn", "kick", "modnick", "mute", "purge", "unban", "unmute", "warn", "warns", "botstatus", "botstatusrepeat", "avatar", "joined", "ping", "quickpoll", "uptime", "userinfo", "add", "choose", "f", "twisted_msg", "untwisted_msg", "scan_url", "vt_hash", "updatecheck", "updatebot", "updatecogs", "loadcog", "lockdownbot", "reloadcog", "restartbot", "shutdownbot", "unloadcog", "help", "emote", "servers", "getchannels", "getinvite"]
