b44f66a0e99f59c098380f1415a618614a7345d8
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-06-06 15:55:45 +0200

    the one tab broke everyone bruh

--- cogs/general.py
+++ cogs/general.py
@@ -14,23 +14,23 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        @commands.command(description='Shows information about bot instance.')
-        async def about(self, ctx):
-            em = discord.Embed(title = "About this instance", color = discord.Color.green())
-            em.add_field(name = "Project URL", value = "https://github.com/FOSS-Devs/freediscord/")
-            em.add_field(name = "Support server", value = "https://discord.gg/myzbqnVUFN")
-            em.add_field(name = "Main bot invite link", value = "Coming soon")
-            servers = list(self.bot.guilds)
-            serverNumber = len(servers)
-            em.add_field(name = "Number of servers this instance is in", value = serverNumber)
-            cpuUsage = psutil.cpu_percent()
-            em.add_field(name = "CPU usage of host", value = cpuUsage)
-            em.add_field(name = "Ping", value = "`"f"{round(self.bot.latency*1000)} ms`")
-            current_time = time.time()
-            difference = int(round(current_time - start_time))
-            text = str(datetime.timedelta(seconds=difference))
-            em.add_field(name="Uptime", value=text)
-            await ctx.send(embed = em)
+    @commands.command(description='Shows information about bot instance.')
+    async def about(self, ctx):
+        em = discord.Embed(title = "About this instance", color = discord.Color.green())
+        em.add_field(name = "Project URL", value = "https://github.com/FOSS-Devs/freediscord/")
+        em.add_field(name = "Support server", value = "https://discord.gg/myzbqnVUFN")
+        em.add_field(name = "Main bot invite link", value = "Coming soon")
+        servers = list(self.bot.guilds)
+        serverNumber = len(servers)
+        em.add_field(name = "Number of servers this instance is in", value = serverNumber)
+        cpuUsage = psutil.cpu_percent()
+        em.add_field(name = "CPU usage of host", value = cpuUsage)
+        em.add_field(name = "Ping", value = "`"f"{round(self.bot.latency*1000)} ms`")
+        current_time = time.time()
+        difference = int(round(current_time - start_time))
+        text = str(datetime.timedelta(seconds=difference))
+        em.add_field(name="Uptime", value=text)
+        await ctx.send(embed = em)
 
 def setup(bot):
     bot.add_cog(General(bot))
--- cogs/update.py
+++ cogs/update.py
@@ -35,117 +35,117 @@ class Update(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        @commands.command()
-        async def updatecheck(self, ctx):
-            if str(ctx.message.author.id) == config.ownerID:
-                firstem = discord.Embed(title = "FOSSDiscord is checking for updates...", color = discord.Color.orange())
-                embedmsg = await ctx.send(embed=firstem)
+    @commands.command()
+    async def updatecheck(self, ctx):
+        if str(ctx.message.author.id) == config.ownerID:
+            firstem = discord.Embed(title = "FOSSDiscord is checking for updates...", color = discord.Color.orange())
+            embedmsg = await ctx.send(embed=firstem)
+            latestversionresponse = requests.get("https://api.github.com/repos/FOSS-Devs/fossdiscord/releases/latest")
+            latestversionget = latestversionresponse.json()["name"]
+            latestversion = latestversionget.split(' ', 1)[1]
+            if globalconfig.currentversion == latestversion:
+                secondem = discord.Embed(title = "Updatecheck\n----------------", color = discord.Color.green())
+                secondem.add_field(name = "Checking for updates succeeded!", value = "There are no updates available.")
+                await embedmsg.edit(embed=secondem)
+            elif globalconfig.currentversion > latestversion:
+                secondem = discord.Embed(title = "Updatecheck\n----------------", color = discord.Color.green())
+                secondem.add_field(name = "Invalid version in the globalconfig.", value = "There is an invalid version in the globalconfig, try downloading a fresh copy of FOSSDiscord.")
+                await embedmsg.edit(embed=secondem)
+            else:
+                secondem = discord.Embed(title = "Updatecheck\n----------------", color = discord.Color.green())
+                secondem.add_field(name = "Checking for updates succeeded!", value = f"The bot can be updated from {globalconfig.currentversion} to {latestversion}.\nYou can update the bot with {config.prefix}updatebot.")
+                await embedmsg.edit(embed=secondem)
+        else:
+            em = discord.Embed(title = "This command is for the bot owner only.", color = discord.Color.red())
+            await ctx.send(embed = em)
+
+    @commands.command()
+    async def updatebot(self, ctx):
+        if str(ctx.message.author.id) == config.ownerID:
+            if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "win32":
                 latestversionresponse = requests.get("https://api.github.com/repos/FOSS-Devs/fossdiscord/releases/latest")
                 latestversionget = latestversionresponse.json()["name"]
                 latestversion = latestversionget.split(' ', 1)[1]
                 if globalconfig.currentversion == latestversion:
-                    secondem = discord.Embed(title = "Updatecheck\n----------------", color = discord.Color.green())
-                    secondem.add_field(name = "Checking for updates succeeded!", value = "There are no updates available.")
-                    await embedmsg.edit(embed=secondem)
+                    em = discord.Embed(title = "Updatebot\n-------------", color = discord.Color.green())
+                    em.add_field(name = 'Already the latest version.', value = "There is no need to update.")
+                    await ctx.send(embed=em)
                 elif globalconfig.currentversion > latestversion:
-                    secondem = discord.Embed(title = "Updatecheck\n----------------", color = discord.Color.green())
-                    secondem.add_field(name = "Invalid version in the globalconfig.", value = "There is an invalid version in the globalconfig, try downloading a fresh copy of FOSSDiscord.")
-                    await embedmsg.edit(embed=secondem)
+                    em = discord.Embed(title = "Updatebot\n-------------", color = discord.Color.green())
+                    em.add_field(name = 'Invalid version in the globalconfig.', value = "There is an invalid version in the globalconfig, try downloading a fresh copy of FOSSDiscord.")
+                    await ctx.send(embed=em)
                 else:
-                    secondem = discord.Embed(title = "Updatecheck\n----------------", color = discord.Color.green())
-                    secondem.add_field(name = "Checking for updates succeeded!", value = f"The bot can be updated from {globalconfig.currentversion} to {latestversion}.\nYou can update the bot with {config.prefix}updatebot.")
-                    await embedmsg.edit(embed=secondem)
-            else:
-                em = discord.Embed(title = "This command is for the bot owner only.", color = discord.Color.red())
-                await ctx.send(embed = em)
+                    datetimenow = datetime.now()
+                    currentdate = datetime.strftime(datetimenow, '%a %b %y %I%M%p')
 
-        @commands.command()
-        async def updatebot(self, ctx):
-            if str(ctx.message.author.id) == config.ownerID:
-            if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "win32":
                     latestversionresponse = requests.get("https://api.github.com/repos/FOSS-Devs/fossdiscord/releases/latest")
                     latestversionget = latestversionresponse.json()["name"]
                     latestversion = latestversionget.split(' ', 1)[1]
-                    if globalconfig.currentversion == latestversion:
-                        em = discord.Embed(title = "Updatebot\n-------------", color = discord.Color.green())
-                        em.add_field(name = 'Already the latest version.', value = "There is no need to update.")
-                        await ctx.send(embed=em)
-                    elif globalconfig.currentversion > latestversion:
-                        em = discord.Embed(title = "Updatebot\n-------------", color = discord.Color.green())
-                        em.add_field(name = 'Invalid version in the globalconfig.', value = "There is an invalid version in the globalconfig, try downloading a fresh copy of FOSSDiscord.")
-                        await ctx.send(embed=em)
-                    else:
-                        datetimenow = datetime.now()
-                        currentdate = datetime.strftime(datetimenow, '%a %b %y %I%M%p')
 
-                        latestversionresponse = requests.get("https://api.github.com/repos/FOSS-Devs/fossdiscord/releases/latest")
-                        latestversionget = latestversionresponse.json()["name"]
-                        latestversion = latestversionget.split(' ', 1)[1]
+                    downloadurl = f"https://codeload.github.com/FOSS-Devs/fossdiscord/zip/v{latestversion}"
+                    currentdir = os.getcwd()
 
-                        downloadurl = f"https://codeload.github.com/FOSS-Devs/fossdiscord/zip/v{latestversion}"
-                        currentdir = os.getcwd()
+                    os.mkdir(f'{currentdir}/updatertemp')
+                    os.mkdir(f'{currentdir}/updatertemp/unzipped')
+                    wget.download(downloadurl, f'{currentdir}/updatertemp/download.zip')
 
-                        os.mkdir(f'{currentdir}/updatertemp')
-                        os.mkdir(f'{currentdir}/updatertemp/unzipped')
-                        wget.download(downloadurl, f'{currentdir}/updatertemp/download.zip')
+                    with ZipFile(f'{currentdir}/updatertemp/download.zip', 'r') as zipObj:
+                        zipObj.extractall(f'{currentdir}/updatertemp/unzipped')
 
-                        with ZipFile(f'{currentdir}/updatertemp/download.zip', 'r') as zipObj:
-                            zipObj.extractall(f'{currentdir}/updatertemp/unzipped')
+                    os.mkdir(f'{currentdir}/backup-{currentdate}')
 
-                        os.mkdir(f'{currentdir}/backup-{currentdate}')
+                    allfiles = ["bot.py", "setup.py", "cogs", "docs", "src", "README.md", ".gitignore", "LICENSE", "globalconfig.py"]
 
-                        allfiles = ["bot.py", "setup.py", "cogs", "docs", "src", "README.md", ".gitignore", "LICENSE", "globalconfig.py"]
+                    for f in allfiles:
+                        shutil.move(f'{currentdir}/' + f, f'{currentdir}/backup-{currentdate}')
 
-                        for f in allfiles:
-                            shutil.move(f'{currentdir}/' + f, f'{currentdir}/backup-{currentdate}')
+                    allfilesunzipped = os.listdir(f'{currentdir}/updatertemp/unzipped/fossdiscord-{latestversion}')
 
-                        allfilesunzipped = os.listdir(f'{currentdir}/updatertemp/unzipped/fossdiscord-{latestversion}')
+                    for f in allfilesunzipped:
+                        shutil.move(f'{currentdir}/updatertemp/unzipped/fossdiscord-{latestversion}/' + f, currentdir)
 
-                        for f in allfilesunzipped:
-                            shutil.move(f'{currentdir}/updatertemp/unzipped/fossdiscord-{latestversion}/' + f, currentdir)
-
-                        shutil.rmtree("updatertemp")
-                        print("Done! Restart the bot to apply the changes!")
-                        em = discord.Embed(title = "Updated!", description = "FOSSDiscord updated! No error reported. Check your console to confirm this.", color = discord.Color.green())
-                        em.add_field(name = "Note", value = "You have to start the bot again manually. If the bot errors while starting, open an issue in FOSSDiscord's GitHub repository.")
-                        await ctx.send(embed = em)
-                        #close_port()
-                        await ctx.bot.close()
-            elif sys.platform == "darwin":
-                    em = discord.Embed(title = "We are still testing `updatebot` for macOS.", color = discord.Color.red())
+                    shutil.rmtree("updatertemp")
+                    print("Done! Restart the bot to apply the changes!")
+                    em = discord.Embed(title = "Updated!", description = "FOSSDiscord updated! No error reported. Check your console to confirm this.", color = discord.Color.green())
+                    em.add_field(name = "Note", value = "You have to start the bot again manually. If the bot errors while starting, open an issue in FOSSDiscord's GitHub repository.")
                     await ctx.send(embed = em)
-
-            else:
-                em = discord.Embed(title = "This command is for the bot owner only.", color = discord.Color.red())
+                    #close_port()
+                    await ctx.bot.close()
+        elif sys.platform == "darwin":
+                em = discord.Embed(title = "We are still testing `updatebot` for macOS.", color = discord.Color.red())
                 await ctx.send(embed = em)
 
-        # @commands.command()
-        # async def updatecogs(self, ctx):
-        #     """Updates cogs, but not the bot."""
-        #     if str(ctx.message.author.id) == config.ownerID:
-        #         # username = os.getlogin()
-        #         try:
-        #             os.mkdir('/tmp/cogupdate')
-        #         except OSError:
-        #             os.rmdir('/tmp/cogupdate')
-        #             os.mkdir('/tmp/cogupdate')
-        #         HTTPS_REMOTE_URL = globalconfig.github_login_url
-        #         DEST_NAME = '/tmp/cogupdate'
-        #         cloned_repo = Repo.clone_from(HTTPS_REMOTE_URL, DEST_NAME)
-        #         dir_path = os.getcwd()
-        #         shutil.rmtree(dir_path + "/cogs/")
-        #         path = dir_path
-        #         src = '/tmp/cogupdate/cogs'
-        #         dest = dir_path + "/cogs"
-        #         destination = shutil.copytree(src, dest)
-        #         shutil.rmtree('/tmp/cogupdate')
-        #         print("Done! Restart the bot to apply the changes!")
-        #         em = discord.Embed(title = "Updated!", description = "Cogs updated! No error reported. Check your console to confirm this.", color = discord.Color.green())
-        #         em.add_field(name = "Note", value = "If you want to use the new cogs, either restart the bot using `" + config.prefix + "restartbot` which will load all the cogs on startup (recommended), or reload every cog manually using `" + config.prefix + "reloadcog {every cog name}.`")
-        #         await ctx.send(embed = em)
-        #     else:
-        #         em = discord.Embed(title = "This command is for the bot owner only.", color = discord.Color.red())
-        #         await ctx.send(embed = em)
+        else:
+            em = discord.Embed(title = "This command is for the bot owner only.", color = discord.Color.red())
+            await ctx.send(embed = em)
+
+    # @commands.command()
+    # async def updatecogs(self, ctx):
+    #     """Updates cogs, but not the bot."""
+    #     if str(ctx.message.author.id) == config.ownerID:
+    #         # username = os.getlogin()
+    #         try:
+    #             os.mkdir('/tmp/cogupdate')
+    #         except OSError:
+    #             os.rmdir('/tmp/cogupdate')
+    #             os.mkdir('/tmp/cogupdate')
+    #         HTTPS_REMOTE_URL = globalconfig.github_login_url
+    #         DEST_NAME = '/tmp/cogupdate'
+    #         cloned_repo = Repo.clone_from(HTTPS_REMOTE_URL, DEST_NAME)
+    #         dir_path = os.getcwd()
+    #         shutil.rmtree(dir_path + "/cogs/")
+    #         path = dir_path
+    #         src = '/tmp/cogupdate/cogs'
+    #         dest = dir_path + "/cogs"
+    #         destination = shutil.copytree(src, dest)
+    #         shutil.rmtree('/tmp/cogupdate')
+    #         print("Done! Restart the bot to apply the changes!")
+    #         em = discord.Embed(title = "Updated!", description = "Cogs updated! No error reported. Check your console to confirm this.", color = discord.Color.green())
+    #         em.add_field(name = "Note", value = "If you want to use the new cogs, either restart the bot using `" + config.prefix + "restartbot` which will load all the cogs on startup (recommended), or reload every cog manually using `" + config.prefix + "reloadcog {every cog name}.`")
+    #         await ctx.send(embed = em)
+    #     else:
+    #         em = discord.Embed(title = "This command is for the bot owner only.", color = discord.Color.red())
+    #         await ctx.send(embed = em)
 
 
 def setup(bot):
