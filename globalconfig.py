1c540e9df3b76f050bd71bfd4e6714afc01b9a96
Sven <68018116+SKBotNL@users.noreply.github.com>
2021-06-06 12:59:16 +0200

    Fix update loop

--- globalconfig.py
+++ globalconfig.py
@@ -3,4 +3,4 @@
 
 # Command list
 commands = ["about", "ban", "changenick", "delwarn", "kick", "modnick", "mute", "purge", "unban", "unmute", "warn", "warns", "botstatus", "botstatusrepeat", "avatar", "joined", "ping", "quickpoll", "uptime", "userinfo", "add", "choose", "f", "twisted_msg", "untwisted_msg", "scan_url", "vt_hash", "updatecheck", "updatebot", "updatecogs", "loadcog", "lockdownbot", "reloadcog", "restartbot", "shutdownbot", "unloadcog", "help", "emote", "servers", "getchannels", "getinvite"]
-currentversion = '4.3.2'
+currentversion = '4.4.1'
