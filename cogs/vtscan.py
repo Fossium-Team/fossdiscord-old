de9600f29dadb42bb9f52aef8f1cb282b3478289
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-06-12 03:07:05 +0800

    Now, the hashcheck, re-analyzing the file.

--- cogs/vtscan.py
+++ cogs/vtscan.py
@@ -19,25 +19,39 @@ class VT(commands.Cog):
         await ctx.message.delete()
         hash = hash.replace(' ', '')
         header = {'x-apikey': f'{apikey}'}
-        vturl = f"https://www.virustotal.com/api/v3/files/{hash}"
-        response = requests.get(vturl, headers = header).json()
+        vturl = f'https://www.virustotal.com/api/v3/files/{hash}/analyse'
+        response = requests.post(vturl, headers = header).json()
         try:
-            detection = int(response['data']['attributes']['last_analysis_stats']['malicious'])
-            suspicious = int(response['data']['attributes']['last_analysis_stats']['suspicious'])
+            result_id = str(response['data']['id'])
         except Exception:
             response = str(response['error']['code'])
             em = discord.Embed(title = f"Error: `{response}`", color = discord.Color.red())
             em.set_author(name="VirusTotal", icon_url=iconurl)
             await ctx.send(embed = em, delete_after=5.0)
             return
+        em = discord.Embed(title = "Re-analyzing file...", description = "Please wait for 25 seconds.", color = discord.Color.blue())
+        em.set_author(name="VirusTotal", icon_url=iconurl)
+        msg = await ctx.send(embed = em)
+        await asyncio.sleep(25)
+        vturl = f'https://www.virustotal.com/api/v3/analyses/{result_id}'
+        response = requests.get(vturl, headers=header).json()
+        try:
+            detection = int(response['data']['attributes']['stats']['malicious'])
+            suspicious = int(response['data']['attributes']['stats']['suspicious'])
+        except Exception:
+            response = str(response['error']['code'])
+            new_embed = discord.Embed(title = f"Error: `{response}`", color = discord.Color.red())
+            new_embed.set_author(name="VirusTotal", icon_url=iconurl)
+            await msg.edit(embed=new_embed, delete_after=5.0)
+            return
         generated_link = f"https://www.virustotal.com/gui/file/{hash}/detection"
         if detection >= 1 or suspicious >= 1:
-            em = discord.Embed(title = f"Detections: `{detection}`\nDetected as suspicious: `{suspicious}`", color = discord.Color.red())
+            new_embed = discord.Embed(title = f"Detections: `{detection}`\nDetected as suspicious: `{suspicious}`", color = discord.Color.red())
         else:
-            em = discord.Embed(title = f"The file looks clean, detections: {detection}", color = discord.Color.green())
-        em.set_author(name="VirusTotal", icon_url=iconurl)
-        em.add_field(name="Link:", value=generated_link)
-        await ctx.send(embed = em)
+            new_embed = discord.Embed(title = f"Detections: `{detection}`, the file should be clean.", color = discord.Color.green())
+        new_embed.set_author(name="VirusTotal", icon_url=iconurl)
+        new_embed.add_field(name="Link:", value=generated_link)
+        await msg.edit(embed=new_embed)
 
     @commands.max_concurrency(1, per=commands.BucketType.guild, wait=False)
     @commands.cooldown(1, 45, commands.BucketType.user)
@@ -79,12 +93,10 @@ class VT(commands.Cog):
             category = response['data']['attributes']["categories"]
             for attr, value in category.items():
                 new_embed.add_field(name = f"{attr}", value = f"`{value}`")
-            new_embed.set_author(name="VirusTotal", icon_url=iconurl)
-            new_embed.add_field(name="Link:", value=generated_link)
         else:
             new_embed = discord.Embed(title = f"Detections: `{detection}`, the website should be clean.", color = discord.Color.green())
-            new_embed.set_author(name="VirusTotal", icon_url=iconurl)
-            new_embed.add_field(name="Link:", value=generated_link)
+        new_embed.set_author(name="VirusTotal", icon_url=iconurl)
+        new_embed.add_field(name="Link:", value=generated_link)
         await msg.edit(embed=new_embed)
 
 
