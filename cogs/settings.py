79fd7f27c13bd000cedd5559130f18950da76684
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-06-13 02:49:59 +0800

    Try this.

--- cogs/settings.py
+++ cogs/settings.py
@@ -82,19 +82,20 @@ class Settings(commands.Cog):
                         lst.append(attr)
                     usernum = str(lst[-1])
                     usernum = int(''.join(filter(str.isdigit, str(usernum)))) + 1
-                    writeblacklist = blacklistjson.update({"data":{f"user{usernum}": f'{userid}'}})
+                    blacklistjson["data"][f"user{usernum}"] = f'{userid}'
+                    #writeblacklist = blacklistjson.update({"data":{f"user{usernum}": f'{userid}'}})
                     #blacklisted = blacklistjson['blacklist']
                     #blacklist = f'{blacklisted}, {userid}'
                     #riteblacklist = ({"blacklist": f'{blacklist}'})
                     with open("settings/blacklist.json", 'w') as file:
-                        json.dump(writeblacklist, file)
+                        json.dump(blacklistjson, file)
                     em = discord.Embed(title = 'Blacklisted that user.', color = discord.Color.green())
                     await ctx.send(embed=em)
                 
                 elif os.stat("settings/blacklist.json").st_size == 0:
-                    writeblacklist = {"data": {"user0": f'{userid}'}}
+                    blacklistjson = {"data": {"user0": f'{userid}'}}
                     with open("settings/blacklist.json", 'w') as file:
-                        json.dump(writeblacklist, file)
+                        json.dump(blacklistjson, file)
                     em = discord.Embed(title = 'Blacklisted that user.', color = discord.Color.green())
                     await ctx.send(embed=em)
 
