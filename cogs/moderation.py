348063e6b6b1b2a74b220fa50e8bccda54696bea
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-06-13 13:37:26 +0200

    Send message when no reason given warn command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -133,33 +133,37 @@ class Moderation(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def warn(self, ctx, user : discord.Member, *reason):
         args = " ".join(reason[:])
-
-        if not os.path.exists('warns'):
-            os.makedirs('warns')
-        try:
-            if os.stat(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py").st_size > 0:
-                em = discord.Embed(title = "Successfully warned that member.", color = discord.Color.orange())
-                await ctx.send(embed=em)
-                writeReasonTemplate = str(args)
-                warns = open(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py", 'a')
-                warns.write("\n")
-                warns.write(writeReasonTemplate)
-                warns.close()
-
-            elif os.stat(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py").st_size == 0:
-                em = discord.Embed(title = "Successfully warned that member.", color = discord.Color.orange())
-                await ctx.send(embed=em)
-                writeReasonTemplate = str(args)
-                warns = open(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py", 'a')
-                warns.write(writeReasonTemplate)
-                warns.close()
-        except:
-            em = discord.Embed(title = "Successfully warned that member.", color = discord.Color.orange())
+        
+        if args == "":
+            em = discord.Embed(title = "No reason given.", color = discord.Color.red())
             await ctx.send(embed=em)
-            writeReasonTemplate = str(args)
-            warns = open(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py", 'a')
-            warns.write(writeReasonTemplate)
-            warns.close()
+        else:
+            if not os.path.exists('warns'):
+                os.makedirs('warns')
+            try:
+                if os.stat(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py").st_size > 0:
+                    em = discord.Embed(title = "Successfully warned that member.", color = discord.Color.orange())
+                    await ctx.send(embed=em)
+                    writeReasonTemplate = str(args)
+                    warns = open(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py", 'a')
+                    warns.write("\n")
+                    warns.write(writeReasonTemplate)
+                    warns.close()
+
+                elif os.stat(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py").st_size == 0:
+                    em = discord.Embed(title = "Successfully warned that member.", color = discord.Color.orange())
+                    await ctx.send(embed=em)
+                    writeReasonTemplate = str(args)
+                    warns = open(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py", 'a')
+                    warns.write(writeReasonTemplate)
+                    warns.close()
+            except:
+                em = discord.Embed(title = "Successfully warned that member.", color = discord.Color.orange())
+                await ctx.send(embed=em)
+                writeReasonTemplate = str(args)
+                warns = open(f"warns/{str(user.id)}_{str(ctx.message.guild.id)}.py", 'a')
+                warns.write(writeReasonTemplate)
+                warns.close()
 
 
     @commands.command()
