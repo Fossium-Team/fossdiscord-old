a43f74b7491505be178f952fa88d0939d72ae1b6
Sven <68018116+SKBotNL@users.noreply.github.com>
2021-06-13 12:26:47 +0200

    Update globalconfig.py

--- globalconfig.py
+++ globalconfig.py
@@ -1,6 +1,4 @@
 # Copyright (c) 2021 SKBotNL (and the members of the FOSS-Devs)
 # See LICENSE in the project root for license information.
 
-# Command list
-commands = ["about", "ban", "changenick", "delwarn", "kick", "modnick", "mute", "purge", "unban", "unmute", "warn", "warns", "botstatus", "botstatusrepeat", "avatar", "joined", "ping", "quickpoll", "uptime", "userinfo", "add", "choose", "f", "twisted_msg", "untwisted_msg", "scan_url", "vt_hash", "updatecheck", "updatebot", "updatecogs", "loadcog", "lockdownbot", "reloadcog", "restartbot", "shutdownbot", "unloadcog", "help", "emote", "servers", "getchannels", "getinvite"]
-currentversion = '4.4.1'
+currentversion = '5.0'
