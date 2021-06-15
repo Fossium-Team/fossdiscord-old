c043ef9b706524b1cfe33a7cc85192ce7dfc198e
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-06-15 19:42:42 +0800

    See if that looks better.

--- cogs/vtscan.py
+++ cogs/vtscan.py
@@ -93,6 +93,7 @@ class VT(commands.Cog):
         generated_link = f"https://www.virustotal.com/gui/url/{result_id}/detection"
         if detection >= 1 or suspicious >= 1:
             new_embed = discord.Embed(title = f"Scan Result:", color = discord.Color.red())
+            new_embed.add_field(name= '\u200B', value= '\u200B', inline = False)
             #category = response['data']['attributes']["categories"]
             new_embed.add_field(name="Detected As Malicious:", value=f"{detection}", inline=False)
             new_embed.add_field(name="Detected As Suspicious:", value=f"{suspicious}", inline=False)
@@ -100,6 +101,7 @@ class VT(commands.Cog):
             #    new_embed.add_field(name = f"{attr}", value = f"`{value}`", inline=True)
         else:
             new_embed = discord.Embed(title = "The website should be clean.", color = discord.Color.green())
+            new_embed.add_field(name= '\u200B', value= '\u200B', inline = False)
         category = response['data']['attributes']["categories"]
         for attr, value in category.items():
             new_embed.add_field(name = f"{attr}", value = f"`{value}`", inline=True)
