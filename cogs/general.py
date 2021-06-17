ec72c97001dbd761d5f4c3b7833d92c88427ba3e
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-06-17 17:35:45 +0200

    Replaced CPU usage with updates

--- cogs/general.py
+++ cogs/general.py
@@ -3,7 +3,6 @@
 
 import discord
 from discord.ext import commands
-import psutil
 import config
 import bot
 import datetime
@@ -20,13 +19,20 @@ class General(commands.Cog):
     @commands.command(description='Shows information about bot instance.')
     async def about(self, ctx):
         em = discord.Embed(title = "About this instance", color = discord.Color.green())
-        em.add_field(name = "Project URL", value = "https://github.com/FOSS-Devs/freediscord/")
+        em.add_field(name = "Project URL", value = "https://github.com/FOSS-Devs/fossdiscord/")
         em.add_field(name = "Support server", value = "https://discord.gg/myzbqnVUFN")
         servers = list(self.bot.guilds)
         serverNumber = len(servers)
         em.add_field(name = "Number of servers this instance is in", value = serverNumber)
-        cpuUsage = psutil.cpu_percent()
-        em.add_field(name = "CPU usage of host", value = cpuUsage)
+        latestversionresponse = requests.get("https://api.github.com/repos/FOSS-Devs/fossdiscord/releases/latest")
+        latestversionget = latestversionresponse.json()["name"]
+        latestversion = latestversionget.split(' ', 1)[1]
+        if globalconfig.currentversion == latestversion:
+            em.add_field(name = "Updates", value = "There are no updates available.")
+        elif globalconfig.currentversion > latestversion:
+            em.add_field(name = "Updates", value = f"Error while checking for updates, try running {config.prefix}updatecheck.")
+        else:
+            em.add_field(name = "Updates", value = f"You can update the bot from {globalconfig.currentversion} to {latestversion}.\nYou can update the bot with {config.prefix}updatebot.")
         em.add_field(name = "Ping", value = "`"f"{round(self.bot.latency*1000)} ms`")
         current_time = time.time()
         difference = int(round(current_time - start_time))
